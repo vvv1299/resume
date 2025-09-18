1324eef9514cf259c0e3a81e65804e0a056a7fef
Valeria <39722706+vvv1299@users.noreply.github.com>
2025-09-18 08:00:56 +0400

    добавление кнопки "наверх" при прокрутке

---
+++ scripts/up-button.js
@@ -0,0 +1,15 @@
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 50) {
+    console.log(window.pageYOffset);
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
+
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
